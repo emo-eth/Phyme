380f1059b861ae76b267bca775c99b277ffb5b0d
James Wenzel <jameswenzel@berkeley.edu>
2018-02-12 22:02:56 -0800

    add dummy methods to dev

--- src/Phyme.py
+++ src/Phyme.py
@@ -20,3 +20,44 @@ class Phyme(object):
         if results:
             results.remove(word.upper())
         return results
+
+    def get_additive_rhymes(self, word):
+        '''Get additve rhymes (PASS -> FAST)'''
+        # TODO: find legitimate amalgamations of consonants?
+        pass
+
+    def get_subtractive_rhymes(self, word):
+        '''Get subtractive rhymes (FAST -> PASS)'''
+        pass
+
+    def get_family_rhymes(self, word):
+        '''Get rhymes from the same consonant family (type, voiced) (DOG -> COB)
+        Design decisions:
+        - should family rhyme be on stressed or unstressed syllables (or both?) 
+        - how to handle composite consonants
+        '''
+        pass
+
+    def get_partner_rhymes(self, word):
+        '''Get rhymes from the same consonant groups (eg, fricative)'''
+        pass
+
+    def get_assonance_rhymes(self, word):
+        '''Get rhymes that stress the same vowel'''
+        pass
+
+    def get_perfect_vowel_partner_rhymes(self, word):
+        '''Get rhymes using similar vowels with the same consonant'''
+        pass
+
+    def get_family_vowel_partner_partner_rhymes(self, word):
+        '''Get rhymes using similar values with consonant family'''
+        pass
+
+    def get_partner_vowel_partner_rhymes(self, word):
+        '''Get rhymes with similar vowels with same consonant group'''
+        pass
+
+    def get_consonant_rhymes(self, word):
+        '''Get rhymes that end with the same consonant'''
+        pass
