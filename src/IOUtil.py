e89c04917be602056d66c98172ee577057641df2
James Wenzel <jameswenzel@berkeley.edu>
2018-02-12 20:31:45 -0800

    consolidate affricate and fricative consonat families

--- src/IOUtil.py
+++ src/IOUtil.py
@@ -5,6 +5,9 @@ import os
 
 file_path = os.path.dirname(__file__)
 
+AFFRICATE = 'affricate'
+FRICATIVE = 'fricative'
+
 # use global objects so we don't load data each time we call a method
 rt = None
 word_phone_dict = {}
@@ -29,7 +32,8 @@ def load_word_phone_dict():
     if word_phone_dict:
         return word_phone_dict
     # associate words with a list of phonemes
-    with open(os.path.join(file_path, '../cmudict/cmudict-0.7b.txt'), encoding='latin1') as f:
+    with open(os.path.join(file_path, '../cmudict/cmudict-0.7b.txt'),
+              encoding='latin1') as f:
         for line in f:
             if line.startswith(';;;'):
                 continue
@@ -43,14 +47,18 @@ def load_phone_type_dicts():
     '''Load both phone -> type and type -> phone mapped dicts'''
     if phone_type_dict and type_phone_dict:
         return phone_type_dict, type_phone_dict
-    with open(os.path.join(file_path, '../cmudict/cmudict-0.7b.phones.txt'), encoding='latin1') as f:
+    with open(os.path.join(file_path, '../cmudict/cmudict-0.7b.phones.txt'),
+              encoding='latin1') as f:
         for line in f:
-            splits = line.split()
-            word = splits[0]
-            phone_type_dict[splits[0]] = splits[1]
-            if splits[1] == 'vowel':
+            phone, family = line.split()
+            # at the expense of linguistic purity, group affricates and
+            # fricatives together for rhyming purposes
+            if family == AFFRICATE:
+                family = FRICATIVE
+            phone_type_dict[phone] = family
+            if family == 'vowel':
                 # quick lookup for stressed syllables: AY0, AY1, AY2
                 for x in range(3):
-                    phone_type_dict[splits[0] + str(x)] = 'vowel'
-            type_phone_dict[splits[1]].add(splits[0])
+                    phone_type_dict[phone + str(x)] = 'vowel'
+            type_phone_dict[family].add(phone)
     return phone_type_dict, type_phone_dict
