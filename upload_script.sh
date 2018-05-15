17165193da62623fa012ec4d705eebf3350a7fef
jwenzel <jwenzel@pandora.com>
2018-05-15 14:22:31 -0700

    version update

---
+++ upload_script.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+version=$(echo "from Phyme import version; print(version)" | python)
+
+git add .
+git commit -m "version update"
+git push origin master
+git tag $version
+git push --tags origin master
+python setup.py register sdist upload -r pypi
